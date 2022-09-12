f8f82524b310cff2405f539f6f533f0a8e608868
Al Zee <z@alz.ee>
2022-09-12 15:53:49 +0800

    nth

--- itove.js
+++ itove.js
@@ -1,16 +1,29 @@
 // root.html add <script defer type="text/javascript" src="/static/itove.js"></script>
 //
-setTimeout(() => {
+const intv = setInterval(() => {
     let footer = document.querySelector('.hfroute-footer');
-    let copyright = footer.querySelector('.footer-copyright');
-    copyright.innerText = `© ${new Date().getFullYear()} ITOVE Inc.`;
+    if (footer) {
+        let copyright = footer.querySelector('.footer-copyright');
+        if (copyright && copyright.id != 'copyright0') {
+            copyright.innerText = `© ${new Date().getFullYear()} ITOVE Inc.`;
+            copyright.id = 'copyright0';
+        }
 
-    let link = footer.querySelector('a');
-    let icp = link.cloneNode(true);
-    icp.innerText = '鄂ICP备20007274号';
-    icp.href = 'https://beian.miit.gov.cn/';
-    footer.appendChild(icp);
-    console.log(icp);
+        if (footer.querySelector('#icp')) {
+            // back from signup_user_complete/reset_password link to / not /login so everything will reset
+            // so just keep editing?
+            clearInterval(intv);
+        } else{
+            let link = footer.querySelector('a');
+            let icp = link.cloneNode(true);
+            icp.innerText = '鄂ICP备20007274号';
+            icp.href = 'https://beian.miit.gov.cn/';
+            icp.id = 'icp';
+            footer.appendChild(icp);
+        }
 
-}, 5000
+        // TODO reset_passwd page
+        // let footerResetPwd = footer.querySelector('#footer_section');
+    }
+}, 1000
 );
